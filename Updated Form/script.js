c3e6371d70a4b47dcffa5c38584f4e15e7c174bf
Yedluri Rajesh <126222647+yoursrajesh@users.noreply.github.com>
2023-11-03 12:24:41 +0530

    Add files via upload

---
+++ Updated Form/script.js
@@ -0,0 +1,16 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz8rGXkfvBeer90OnOKQZZ9ZV3wf8luGq8WIigThoS2BVZ1ztwJVrg54YuTv_FZ7XFe/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => alert("Thank you! your form is submitted successfully."))
+        .then(() => {
+            window.location.reload();
+        })
+        .catch(error => console.error('Error!', error.message))
+})
